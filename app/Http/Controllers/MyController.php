b859e4c12e04a0d9357f36a06785ea025617085b
bozhevik <danilkasoyka@gmail.com>
2025-04-04 14:22:05 +0200

    что-то сделал

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginReq;           // Запрос для валидации данных при авторизации
 use App\Http\Requests\RegisterReq;      // Запрос для валидации данных при регистрации
-use App\DTO\LoginDTO;          
-use App\DTO\UserDTO; 
-use App\DTO\RegisterDTO; 
-use App\Models\User; 
-use Illuminate\Support\Facades\Auth; 
-use Illuminate\Http\Request; 
-use Illuminate\View\View; 
+use App\DTO\LoginDTO;
+use App\DTO\UserDTO;
+use App\DTO\RegisterDTO;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
 use Illuminate\Support\Facades\Hash; // Для хеширования паролей
 
 class MyController extends Controller
@@ -18,10 +18,10 @@ class MyController extends Controller
       //  регистрации нового юзера
     public function register(RegisterReq $request)
     {
-        
+
     // Получаем DTO с данными из запроса
     $dto = $request->toDTO();
-  
+
     // создаем нового юзера на основе DTO
     $user = User::create([
         'username' => $dto->username,
@@ -43,16 +43,13 @@ class MyController extends Controller
     ], 201);
 
     }
-
-
-
     //  авторизации юзера
     public function login(LoginReq $request)
     {
         $dto = $request->toDTO(); // Преобразуем запрос в DTO
 
         // Проверяем правильность логина и пароля
-        if (!Auth::attempt(['username' => $dto->username, 'password' => $dto->password])) 
+        if (!Auth::attempt(['username' => $dto->username, 'password' => $dto->password]))
         {
             return response()->json(['message' => 'Error 401, oshibka auth'], 401); // Ошибка, если данные неверные
         }
@@ -85,12 +82,12 @@ class MyController extends Controller
         if (!$user) {
             return response()->json(['message' => 'user  not found'], 404);
         }
-    
+
         // Проверяем, есть ли у пользователя токены
         if ($user->tokens()->count() === 0) {
             return response()->json(['message' => 'No auth!'], 403);
         }
-    
+
         // Преобразуем пользователя в DTO и возвращаем данные
         $userDTO = UserDTO::fromModel($user);
         return response()->json($userDTO->toArray(), 200);
@@ -121,12 +118,12 @@ class MyController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], 404);
         }
-    
+
         // Проверяем, есть ли токены
         if ($user->tokens()->count() === 0) {
             return response()->json(['message' => 'User no auth'], 403);
         }
-    
+
         // Формируем список токенов
         $tokens = $user->tokens->map(function ($token) {
             return [
@@ -136,7 +133,7 @@ class MyController extends Controller
                 'created_at' => $token->created_at,
             ];
         });
-    
+
         return response()->json($tokens, 200);
     }
 
@@ -146,14 +143,14 @@ class MyController extends Controller
 
     //  разлогирования (удаления) всех токенов текущего юзера
     public function logoutAll(Request $request)
-    {   
+    {
         $user = $request->user();// Получаем текущего пользователя
 
         // $request->user()->tokens()->delete(); // Удаляем все токены юзера
-       
+
         foreach ($user->tokens as $token) { $token->delete();}
 
-       
+
         return response()->json(['message' => 'All tokens are deleted!!! '], 200); // Сообщение об успешном отзыве всех токенов
     }
 
@@ -170,29 +167,29 @@ class MyController extends Controller
             'new_password' => ['required', 'string', 'min:8', 'regex:/[A-Z]/', 'regex:/[a-z]/', 'regex:/\d/', 'regex:/\W/'], // Новый пароль
             'confirm_new_password' => ['required', 'same:new_password'], // Подтверждение нового пароля
         ]);
-    
+
         // Находим пользователя по имени
         $user = User::where('username', $request->username)->first();
-    
+
         if (!$user) {
             return response()->json(['message' => 'User not fountd'], 404);
         }
-    
+
         // Проверяем, что текущий пароль введен правильно
         if (!Hash::check($request->current_password, $user->password)) {
             return response()->json(['message' => 'Current password is wrong'], 403);
         }
-    
+
         // Обновляем пароль пользователя
         $user->update([
             'password' => bcrypt($request->new_password),
         ]);
-    
+
         // Отзываем все токены пользователя
         $user->tokens()->delete();
-    
+
         return response()->json(['message' => "Password '{$user->username}' changed!"], 200);
     }
-    
-    
+
+
 }
